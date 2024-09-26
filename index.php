834721730b28316e314c82b206da247b6929305f
ChristopherS24 <chryskies@gmail.com>
2024-09-26 17:02:53 +0200

    inserito paragrafo ed una parola da censurare

--- index.php
+++ index.php
@@ -1,11 +1,34 @@
+<?php
+    $name = 'Chris';
+    $message = 'Ciao come va?';
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>PHP Badwords</title>
 </head>
 <body>
+    <h1>Badwords</h1>
+
+    <div>
+        <p>
+            Mi chiamo: <?php echo $name; ?>
+        </p>
+        <p>
+            Il mio messaggio segreto è: <?php echo $message; ?>
+        </p>
+       
+    </div>
     
+    <form action="./about.php">
+
+
+    </form>
+    
+<!--JS-->
+    <script type="text/javascript" src="js/script.js"></script>
 </body>
 </html>
