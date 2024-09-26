b193825621e0859fe4706182428757baf3ec174b
ChristopherS24 <chryskies@gmail.com>
2024-09-26 16:20:58 +0200

    caricato template

--- index.php
+++ index.php
@@ -3,11 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Badwords</title>
+    <title>Document</title>
 </head>
 <body>
     
-<!--JS-->
-    <script type="text/javascript" src="js/script.js"></script>
 </body>
 </html>
